9a6e4b8c771b495b7828a3cb1cb4fe176368fc03
KanchanPatil1 <131779517+KanchanPatil1@users.noreply.github.com>
2023-06-07 19:25:03 +0530

    Add files via upload

---
+++ Avarage.java
@@ -0,0 +1,17 @@
+
+public class Avarage {
+    public static void main(String[] arg){
+      int arr[]={1,2,3,4,5};
+      int sum=0;
+      int avg=0;
+
+      for(int i=0;i<arr.length;i++){
+        sum=sum+arr[i];
+        }
+        avg=sum/arr.length;
+        System.out.println(avg);
+        System.out.println(sum);
+      }
+    }
+
+
